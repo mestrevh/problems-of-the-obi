4aad1cc64ecc2a788737817593e62eb7834e12cb
mestrevh <victor-torugo@hotmail.com>
2026-04-07 07:59:19 -0300

    results: added the 'gabarito'

--- main.py
+++ main.py
@@ -609,25 +609,25 @@ if __name__ == "__main__":
     print("\n🚀 INICIANDO PIPELINE DE AUTOMAÇÃO DA OBI 🚀\n")
 
     # Passo 1: Baixar PDFs
-    #baixar_cadernos_pdf()
+    baixar_cadernos_pdf()
 
     # Passo 2: Mandar para LLM
-    #print(f"\n{'='*40}")
-    #print("2. EXTRAÇÃO DE DADOS (API GEMINI)")
-    #print(f"{'='*40}")
-    #path_data = Path("data")
-    #pdfs_path = list(path_data.rglob("*.pdf"))
+    print(f"\n{'='*40}")
+    print("2. EXTRAÇÃO DE DADOS (API GEMINI)")
+    print(f"{'='*40}")
+    path_data = Path("data")
+    pdfs_path = list(path_data.rglob("*.pdf"))
     
-    #while True:    
-    #    pdfs_path = create_questions(pdfs_path=pdfs_path)
-    #    if len(pdfs_path) == 0:
-    #        break
+    while True:    
+        pdfs_path = create_questions(pdfs_path=pdfs_path)
+        if len(pdfs_path) == 0:
+            break
 
     # Passo 3: Baixar ZIPs de gabaritos
     baixar_gabaritos()
 
     # Passo 4: Cruzar ZIPs com Pastas Output
-    #organizar_test_cases()
+    organizar_test_cases()
 
     # Passo 5: Limpar estrutura dos ZIPs
     #limpar_pastas_test_cases()
