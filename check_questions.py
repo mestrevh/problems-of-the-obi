ae6174197989c96526b524f59fdccdb92d55aa9f
mestrevh <victor-torugo@hotmail.com>
2026-04-09 20:29:46 -0300

    feat: added metrics of the questions extract website obi

--- check_questions.py
+++ check_questions.py
@@ -26,3 +26,23 @@ for question in output.glob("*"):
         df.to_csv("check_questions.csv", index=False, encoding='utf-8')
 
         print("CSV atualizado com sucesso!")
+    
+df = pd.read_csv("check_questions.csv")
+
+counts = {
+    "g": 0,
+    "i": 0,
+}
+
+for indice, linha in df.iterrows():
+    
+    if linha['gabarito'] == "ok":
+        counts['g'] += 1
+    
+    if linha['img'] == "ok":
+        counts['i'] += 1
+
+total = len(df)
+print("Número de questões: ", total)
+print(f"Com gabarito: {counts['g']} | Sem gabarito: {total - counts['g']}")
+print(f"Com imagens: {counts['i']} | Sem imagens: {total - counts['i']}")
